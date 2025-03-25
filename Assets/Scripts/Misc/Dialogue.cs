048398ca067d14b84ec2e25f427e8a0f794a6601
stanlys96 <stanlyskwok@gmail.com>
2025-03-25 18:18:35 +0700

    mostly done

---
+++ Assets/Scripts/Misc/Dialogue.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private Player player;
+
+    public int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        player = FindObjectOfType<Player>();
+        if (player)
+        {
+            player.canMove = false;
+        }
+
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            textComponent.text = string.Empty;
+            gameObject.SetActive(false);
+            if (player)
+            {
+                player.canMove = true;
+                player.PlayerInputIsDisabled = false;
+            }
+        }
+    }
+}
